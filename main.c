382d7e077c37c67a8a5f9a0facd9e19b89c3d731
wxstarlight <45314224+wxstarlight@users.noreply.github.com>
2018-12-20 21:45:19 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,412 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "menu.h"
+
+#define FONTSIZE 10
+int PrintMenuOS()
+{
+    int i, j;
+    char data_M[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "  *    *  ",
+        " ***  *** ",
+        " * *  * * ",
+        " * *  * * ",
+        " *  **  * ",
+        " *      * ",
+        " *      * ",
+        " *      * ",
+        "          "
+    };
+    char data_e[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "          ",
+        "    **    ",
+        "   *  *   ",
+        "  *    *  ",
+        "  ******  ",
+        "  *       ",
+        "   *      ",
+        "    ***   ",
+        "          "
+    };
+    char data_n[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "          ",
+        "    **    ",
+        "   *  *   ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "          "
+    };
+    char data_u[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "          ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "   *  **  ",
+        "    **  * ",
+        "          "
+    };
+    char data_O[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "   ****   ",
+        "  *    *  ",
+        " *      * ",
+        " *      * ",
+        " *      * ",
+        " *      * ",
+        "  *    *  ",
+        "   ****   ",
+        "          "
+    };
+    char data_S[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "    ****  ",
+        "   **     ",
+        "  **      ",
+        "   ***    ",
+        "     **   ",
+        "      **  ",
+        "     **   ",
+        "  ****    ",
+        "          "
+    };
+
+    for(i=0; i<FONTSIZE; i++)
+    {
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_M[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_e[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_n[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_u[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_O[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_S[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int Quit(int argc, char *argv[])
+{
+    /* add XXX clean ops */
+}
+
+#include"syswrapper.h"
+#define MAX_CONNECT_QUEUE   1024
+int Replyhi()
+{
+	char szBuf[MAX_BUF_LEN] = "\0";
+	char szReplyMsg[MAX_BUF_LEN] = "hi\0";
+	InitializeService();
+	while (1)
+	{
+		ServiceStart();
+		RecvMsg(szBuf);
+		SendMsg(szReplyMsg);
+		ServiceStop();
+	}
+	ShutdownService();
+	return 0;
+}
+
+int StartReplyhi(int argc, char *argv[])
+{
+	int pid;
+	/* fork another process */
+	pid = fork();
+	if (pid < 0)
+	{
+		/* error occurred */
+		fprintf(stderr, "Fork Failed!");
+		exit(-1);
+	}
+	else if (pid == 0)
+	{
+		/*	 child process 	*/
+		Replyhi();
+		printf("Reply hi TCP Service Started!\n");
+	}
+	else
+	{
+		/* 	parent process	 */
+		printf("Please input hello...\n");
+	}
+}
+
+int Hello(int argc, char *argv[])
+{
+	char szBuf[MAX_BUF_LEN] = "\0";
+	char szMsg[MAX_BUF_LEN] = "hello\0";
+	OpenRemoteService();
+	SendMsg(szMsg);
+	RecvMsg(szBuf);
+	CloseRemoteService();
+	return 0;
+}
+
+/* File Name: server.c */  
+#include<stdio.h>  
+#include<stdlib.h>  
+#include<string.h>  
+#include<errno.h>  
+#include<sys/types.h>  
+#include<sys/socket.h>  
+#include<netinet/in.h>  
+#define DEFAULT_PORT 8000  
+#define MAXLINE 4096  
+int receive(int argc, char *argv[])  
+{  
+    int    socket_fd, connect_fd;  
+    struct sockaddr_in     servaddr;  
+    char    buff[4096];  
+    int     n;  
+    //初始化Socket  
+    if( (socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){  
+    printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+    //初始化  
+    memset(&servaddr, 0, sizeof(servaddr));  
+    servaddr.sin_family = AF_INET;  
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。  
+    servaddr.sin_port = htons(DEFAULT_PORT);//设置的端口为DEFAULT_PORT  
+  
+    //将本地地址绑定到所创建的套接字上  
+    if( bind(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){  
+    printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+    //开始监听是否有客户端连接  
+    if( listen(socket_fd, 10) == -1){  
+    printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+    printf("======waiting for client's request======\n");  
+    while(1){  
+//阻塞直到有客户端连接，不然多浪费CPU资源。  
+        if( (connect_fd = accept(socket_fd, (struct sockaddr*)NULL, NULL)) == -1){  
+        printf("accept socket error: %s(errno: %d)",strerror(errno),errno);  
+        continue;  
+    }  
+//接受客户端传过来的数据  
+    n = recv(connect_fd, buff, MAXLINE, 0);  
+//向客户端发送回应数据  
+    if(!fork()){ /*紫禁城*/  
+        if(send(connect_fd, "Hello,you are connected!\n", 26,0) == -1)  
+        perror("send error");  
+        close(connect_fd);  
+        exit(0);  
+    }  
+    buff[n] = '\0';  
+    printf("recv msg from client: %s\n", buff);  
+    close(connect_fd);  
+    }  
+    close(socket_fd);  
+}  
+
+
+/* File Name: client.c */  
+  
+#include<stdio.h>  
+#include<stdlib.h>  
+#include<string.h>  
+#include<errno.h>  
+#include<sys/types.h>  
+#include<sys/socket.h>  
+#include<netinet/in.h>  
+  
+#define MAXLINE 4096  
+  
+  
+int send(int argc, char *argv[])  
+{  
+    int    sockfd, n,rec_len;  
+    char    recvline[4096], sendline[4096];  
+    char    buf[MAXLINE];  
+    struct sockaddr_in    servaddr;  
+  
+  
+    if( argc != 2){  
+    printf("usage: ./client <ipaddress>\n");  
+    exit(0);  
+    }  
+  
+  
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){  
+    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);  
+    exit(0);  
+    }  
+  
+  
+    memset(&servaddr, 0, sizeof(servaddr));  
+    servaddr.sin_family = AF_INET;  
+    servaddr.sin_port = htons(8000);  
+    if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){  
+    printf("inet_pton error for %s\n",argv[1]);  
+    exit(0);  
+    }  
+  
+  
+    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){  
+    printf("connect error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+  
+  
+    printf("send msg to server: \n");  
+    fgets(sendline, 4096, stdin);  
+    if( send(sockfd, sendline, strlen(sendline), 0) < 0)  
+    {  
+    printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);  
+    exit(0);  
+    }  
+    if((rec_len = recv(sockfd, buf, MAXLINE,0)) == -1) {  
+       perror("recv error");  
+       exit(1);  
+    }  
+    buf[rec_len]  = '\0';  
+    printf("Received : %s ",buf);  
+    close(sockfd);  
+    exit(0);  
+}  
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+ 
+#include <unistd.h>
+#include <stdio.h>
+#define MAX_IFS 64
+ 
+int BringUpNetInterface()
+{
+    printf("Bring up interface:lo\n");
+    struct sockaddr_in sa;
+    struct ifreq ifreqlo;
+    int fd;
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+    fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    strncpy(ifreqlo.ifr_name, "lo",sizeof("lo"));
+    memcpy((char *) &ifreqlo.ifr_addr, (char *) &sa, sizeof(struct sockaddr));
+    ioctl(fd, SIOCSIFADDR, &ifreqlo);
+    ioctl(fd, SIOCGIFFLAGS, &ifreqlo);
+    ifreqlo.ifr_flags |= IFF_UP|IFF_LOOPBACK|IFF_RUNNING;
+    ioctl(fd, SIOCSIFFLAGS, &ifreqlo);
+    close(fd);
+    
+    printf("Bring up interface:eth0\n");
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr("192.168.40.254");
+    fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    strncpy(ifreqlo.ifr_name, "eth0",sizeof("eth0"));
+    memcpy((char *) &ifreqlo.ifr_addr, (char *) &sa, sizeof(struct sockaddr));
+    ioctl(fd, SIOCSIFADDR, &ifreqlo);
+    ioctl(fd, SIOCGIFFLAGS, &ifreqlo);
+    ifreqlo.ifr_flags |= IFF_UP|IFF_RUNNING;
+    ioctl(fd, SIOCSIFFLAGS, &ifreqlo);
+    close(fd);
+
+    printf("List all interfaces:\n");
+    struct ifreq *ifr, *ifend;
+    struct ifreq ifreq;
+    struct ifconf ifc;
+    struct ifreq ifs[MAX_IFS];
+    int SockFD;
+ 
+ 
+    SockFD = socket(PF_INET, SOCK_DGRAM, 0);
+ 
+ 
+    ifc.ifc_len = sizeof(ifs);
+    ifc.ifc_req = ifs;
+    if (ioctl(SockFD, SIOCGIFCONF, &ifc) < 0)
+    {
+        printf("ioctl(SIOCGIFCONF): %m\n");
+        return 0;
+    }
+ 
+    ifend = ifs + (ifc.ifc_len / sizeof(struct ifreq));
+    for (ifr = ifc.ifc_req; ifr < ifend; ifr++)
+    {
+        printf("interface:%s\n", ifr->ifr_name);
+#if 0
+        if (strcmp(ifr->ifr_name, "lo") == 0)
+        {
+            strncpy(ifreq.ifr_name, ifr->ifr_name,sizeof(ifreq.ifr_name));
+            ifreq.ifr_flags == IFF_UP;
+            if (ioctl (SockFD, SIOCSIFFLAGS, &ifreq) < 0)
+            {
+              printf("SIOCSIFFLAGS(%s): IFF_UP %m\n", ifreq.ifr_name);
+              return 0;
+            }			
+	    }
+#endif
+	    if (ifr->ifr_addr.sa_family == AF_INET)
+        {
+            strncpy(ifreq.ifr_name, ifr->ifr_name,sizeof(ifreq.ifr_name));
+            if (ioctl (SockFD, SIOCGIFHWADDR, &ifreq) < 0)
+            {
+              printf("SIOCGIFHWADDR(%s): %m\n", ifreq.ifr_name);
+              return 0;
+            }
+ 
+            printf("Ip Address %s\n", inet_ntoa( ( (struct sockaddr_in *)  &ifr->ifr_addr)->sin_addr)); 
+            printf("Device %s -> Ethernet %02x:%02x:%02x:%02x:%02x:%02x\n", ifreq.ifr_name,
+                (int) ((unsigned char *) &ifreq.ifr_hwaddr.sa_data)[0],
+                (int) ((unsigned char *) &ifreq.ifr_hwaddr.sa_data)[1],
+                (int) ((unsigned char *) &ifreq.ifr_hwaddr.sa_data)[2],
+                (int) ((unsigned char *) &ifreq.ifr_hwaddr.sa_data)[3],
+                (int) ((unsigned char *) &ifreq.ifr_hwaddr.sa_data)[4],
+                (int) ((unsigned char *) &ifreq.ifr_hwaddr.sa_data)[5]);
+        }
+    }
+ 
+    return 0;
+}
+int main()
+{
+    BringUpNetInterface();
+    PrintMenuOS();
+    SetPrompt("MenuOS>>");
+    MenuConfig("version","MenuOS V1.0(Based on Linux 3.18.6)",NULL);
+    MenuConfig("quit","Quit from MenuOS",Quit);
+    MenuConfig("replyhi", "Reply hi TCP Service", StartReplyhi);
+    MenuConfig("hello", "Hello TCP Client", Hello);
+    MenuConfig("udpservice","send in UDP Service",send);
+	MenuConfig("udpclient","receive in UDP Client",receive); 
+    ExecuteMenu();
+}
